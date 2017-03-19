d95929e380b235f7b8214477ac03e388da4ef988
bubsnow <bubsnow@gmail.com>
2017-03-19 17:13:23 +0800

    Change image url to one stored in database

--- resources/views/_food_card.blade.php
+++ resources/views/_food_card.blade.php
@@ -1,7 +1,7 @@
 <div class="card col-lg-3 col-md-3 col-xs-2 col-lg-offset-1 col-md-offset-1 col-xs-offset-1" style="margin-top: 30px">
     <!--Card image-->
     <div class="view overlay hm-white-slight">
-        <img src="http://ghk.h-cdn.co/assets/cm/15/11/54fe25783596c-cajun-rice-chicken-orig-master-1.jpg" class="img-fluid" alt="food">
+        <img src="/img/food/{{$food->imgurl}}.jpg" class="img-fluid" alt="food">
         <a href="#">
             <div class="mask waves-effect waves-light"></div>
         </a>
