5806e6c617bf533694649aab6d245383c84a9a4b
Julian Chan <frozzenhart@hotmail.com>
2017-03-20 11:40:13 +0800

    more attempted fixes

--- app/Order.php
+++ app/Order.php
@@ -8,6 +8,7 @@ class Order extends Model
 {
     //
     protected $fillable = [
+        'buyer_id','seller_id','food_id',
         'buyer_feedback','deliverer_feedback',
         'buyer_rates','deliverer_rates'
     ];
