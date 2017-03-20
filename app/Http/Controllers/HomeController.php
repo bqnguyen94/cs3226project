f179c9dd37aeabf253adb24f11de281a8a0f6875
Julian Chan <frozzenhart@hotmail.com>
2017-03-20 11:26:12 +0800

    More attempted bugfixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $order = null;
+        return view('index')->with(compact('order'));
     }
 
 }
