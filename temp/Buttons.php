c9e566f4356851cddc38c8495d7eef8ca0e62061
bubsnow <bubsnow@gmail.com>
2017-03-22 00:12:14 +0800

    Codes for button. Original, with hover, plain version without hover

---
+++ temp/Buttons.php
@@ -0,0 +1,11 @@
+
+<a href="menu"><img src="/img/icons/menu2.png" style="border-radius:30px;height:auto;width:auto;max-height:120px;max-width:200px"></a>
+
+<a href="cart"><img src="/img/icons/cart2.png" style="border-radius:30px;height:auto;width:auto;max-height:120px;max-width:200px"></a>
+
+//Image with hover
+<a href="menu"><img class="menu" src="/img/icons/menuHover.png" onmouseover="this.src='/img/icons/menu2.png'" onmouseout="this.src='/img/icons/menuHover.png'" style="border-radius:30px;height:auto;width:auto;max-height:120px;max-width:200px;border-style:solid;border-width:2px;border-color:pink"></a>
+
+//Display only clean image
+<a href="cart"><img src="/img/icons/cartHover.png" style="border-radius:30px;height:auto;width:auto;max-height:120px;max-width:200px;border-style:solid;border-width:2px;border-color:pink"></a>
+
