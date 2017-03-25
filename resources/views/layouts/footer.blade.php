230f763e473bf4c1cdf4d1b6d21a790927a45557
bubsnow <bubsnow@gmail.com>
2017-03-25 14:03:31 +0800

    new footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
+<!--
 <div class="footer">
     <div class="container">
         <div class="row">
@@ -19,3 +20,19 @@
         </div>
     </div>
 </div>
+-->
+
+<footer>
+@yield('link')
+  <ul>
+ 	<li><a href="http://www.nusfood.com/">NUS Food</a></li>
+    <li><a href="/contact">Contact Us</a></li>
+    <li><a href="/about">About Us</a></li>
+    <li><a href="/help">Help</a></li>
+    <li><a href="http://steps.comp.nus.edu.sg/">STePS 10</a></li>
+    <li><a href="http://www.comp.nus.edu.sg/~stevenha/cs3226.html#news">CS3226</a></li>
+    
+  </ul>
+	
+	<span id="copyright"> Prototype &copy 2017 NUS Food. All rights reserved.</span>
+ </footer>
