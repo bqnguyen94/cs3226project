fe33fac37becf37ba1022cf89f6e4a830414dde8
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2017-03-25 20:51:12 +0800

    Add auto bottom scrolling to messages page

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    setInterval( function() { getChatText(); }, 2000);
+    setInterval( function() { getChatText(); }, 1000);
+    $(".panel-body").stop().animate({ scrollTop: $(".panel-body")[0].scrollHeight}, 0);
     $('#btn-send-reply').on("click", function(e) {
         e.preventDefault();
         $.ajaxSetup({
@@ -50,9 +51,13 @@ function getChatText() {
       if (result.message_id > last_time_id) {
           html += '<li class="media"><div class="media-body"><a class="pull-left" href=""><img class="media-object img-circle"src="https:\/\/image.flaticon.com/icons/png/512/78/78373.png" height="60px" width="60px" style="margin-right: 10px"/></a><div class="media-body">' + result.sender_name + '<br/>' + result.message + '</div></div></li><hr/>'
           last_time_id = result.message_id;
+          $('#message-list').append(function() {
+              return $(html).hide().fadeIn();
+          });
+          $(".panel-body").stop().animate({ scrollTop: $(".panel-body")[0].scrollHeight}, 1000);
       }
     }
-    $('#message-list').append(html);
+
   });
 
 }
