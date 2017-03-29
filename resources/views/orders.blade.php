0c0eeecea8903420d0aa0d6e591c775f89ffb05d
bubsnow <bubsnow@gmail.com>
2017-03-29 21:58:21 +0800

    Added banner for orders page

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.template')
 @section('main')
+
+
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Vollkorn" rel="stylesheet"> 
+	
+</head>
+<body>
+<div id="order_heading_background" class="col-xs-12">
+
+	 <p id="order_heading" >Orders</p>
+			
+</div>
+	
+	
 <div class="container">
     <div class="row">
         @foreach ($orders as $order)
@@ -10,3 +24,34 @@
     </div>
 </div>
 @endsection
+
+	
+	
+		
+<style>
+	#order_heading_background{
+				
+		background-image:url("http://139.59.104.3/img/banner/banner_order.png");
+		width:100%;
+		max-height:140px;
+		min-height:105px;
+		margin-bottom:40px;
+	}
+
+	
+	#order_heading{
+		
+		padding-top:20px;
+		text-align:50%;
+		font-size:48px;
+		text-align:center;
+		color:white;
+		font-weight:900;
+		font-family:'Belleza', sans-serif;
+		text-shadow:2px 2px black;
+		
+		
+		
+	}
+
+</style>
