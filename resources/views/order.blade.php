ebd268de957a9132b2561b7782ff494663911e50
Julian Chan <frozzenhart@hotmail.com>
2017-04-03 01:31:03 +0800

    Amendments to page and route

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -32,10 +32,10 @@
         @endif
         @if(Auth::user()->id == deliverer_id)
             @if($order->is_delivered)
-                {!! Form::open(['route'=>'confirm.deliver',$Auth::user()->id,$order->id]) !!}
+                {!! Form::open(['route'=>'confirm.deliver',$order]) !!}
                 {!! Form::close() !!}
             @else
-                {!! Form::open(['route'=>'unconfirm.deliver',$Auth::user()->id, $order->id]) !!}}
+                {!! Form::open(['route'=>'unconfirm.deliver',$order]) !!}}
                 {!! Form::close() !!}
         @endif
         <br/>
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::get('orders', 'OrderController@orders');
 
 Route::get('order/{id}', 'OrderController@order');
 
-Route::post('order/{id}/{order}/confirm-deliver',['as'=>'confirm.deliver','uses'=>'OrderController@confirmDeliver']);
+Route::post('order/{order}/confirm-deliver',['as'=>'confirm.deliver','uses'=>'OrderController@confirmDeliver']);
 
-Route::post('order/{id}/{order}/unconfirm-deliver',['as'=>'unconfirm.deliver','uses'=>'OrderController@unconfirmDeliver']);
+Route::post('order/{order}/unconfirm-deliver',['as'=>'unconfirm.deliver','uses'=>'OrderController@unconfirmDeliver']);
 
 Route::post('order/{id}', 'UserController@accept_offer');
 
