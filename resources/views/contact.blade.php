d4ff8a476720d281a8b5d4d84ced49f33daa7c88
bubsnow <bubsnow@gmail.com>
2017-04-02 15:46:23 +0800

    Contact up - update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,11 +2,7 @@
 @section('main')
 
 <div class="container">
-	<h2 >NUS Food</h2>
 	
-	<h3 >Description</h3>
-	<h5 class="col-xs-12">NUSFood is an online food ordering system that sells to mainly NUS students and staffs. Food is then delivered from restaurants and food stalls within school campus by students for other students. Our focus is on providing a more affordable mode of delivery, as compared to more invested companies such as Deliveroo, and help students who do not have time to queue up to acquire their food.</h5>
-
 	<h3 >Feedbacks</h3>
 	<h5 class="col-xs-12">Currently, we are still at prototying stage. If you do have any feedback, kindly drop us a <a href="/chat/1">chat</a> after signing up for an account.</h5>
 
--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,7 @@ Route::post('messages/{id}', 'MessageController@reply');
 Route::get('refreshmessages/{id}', 'MessageController@refresh_messages');
 
 Route::get('/contact', function(){return view('contact');});
+
+Route::get('/about', function(){return view('about');});
+
+Route::get('/help', function(){return view('help');});
