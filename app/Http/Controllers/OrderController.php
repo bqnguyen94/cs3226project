ec596ef6c413f4054bbac8aa840a9f30de92beb7
Julian Chan <frozzenhart@hotmail.com>
2017-04-03 12:30:35 +0800

    bug fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Order;
 use App\User;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
