0ade30da909f609586421a798693cbc6de2efbc9
Julian Chan <frozzenhart@hotmail.com>
2017-04-03 14:52:40 +0800

    To ensure that file has been committed properly

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -30,7 +30,7 @@
                 <h4>Delivered: Not yet received</h4>
             @endif
         @endif
-
+        
         @if(Auth::user()->id == $order->deliverer_id)
             @if($order->is_delivered)
                      <h4>Delivery has already been confirmed</h4>
