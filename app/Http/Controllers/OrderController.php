75f79b8ccd2eba26f5411620591f0cda00a8ef5c
Julian Chan <frozzenhart@hotmail.com>
2017-04-03 12:32:26 +0800

    another

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Order;
 use App\User;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
 class OrderController extends Controller
 {
