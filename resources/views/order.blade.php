43d053ab636593e152f205a62f3a2a8b37d8cbed
Julian Chan <frozzenhart@hotmail.com>
2017-04-03 11:33:33 +0800

    more fixes

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -32,10 +32,10 @@
         @endif
         @if(Auth::user()->id == $order->deliverer_id)
             @if($order->is_delivered)
-                {!! Form::open(['route'=>'confirm.deliver',$order]) !!}
+                {!! Form::open(['route'=>['confirm.deliver',$order]]) !!}
                 {!! Form::close() !!}
             @else
-                {!! Form::open(['route'=>'unconfirm.deliver',$order]) !!}}
+                {!! Form::open(['route'=>['unconfirm.deliver',$order]]) !!}}
                 {!! Form::close() !!}
             @endif
         @endif
