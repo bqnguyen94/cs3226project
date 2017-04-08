d0b52db95353ec1e0a444953c4b3c4a4e5927ef0
bqnguyen94 <ryuu.sakamoto.164@outlook.com>
2017-04-08 20:50:59 +0800

    Add food image

--- resources/views/_food_card.blade.php
+++ resources/views/_food_card.blade.php
@@ -1,7 +1,7 @@
 <div class="card col-lg-3 col-md-3 col-xs-5 col-lg-offset-1 col-md-offset-1 col-xs-offset-1" style="margin-top: 30px">
     <!--Card image-->
     <div class="view overlay hm-white-slight">
-        <img src="http://ghk.h-cdn.co/assets/cm/15/11/54fe25783596c-cajun-rice-chicken-orig-master-1.jpg" class="img-fluid" alt="food">
+        <img src="<?php echo $food->image ?>" class="img-fluid" alt="food">
         <a href="#">
             <div class="mask waves-effect waves-light"></div>
         </a>
@@ -13,9 +13,9 @@
         <!--Title-->
         <h4 class="card-title">{{ $food->name }}</h4>
         <!--Text-->
-        <p class="card-text">Price: 
-			<?php 
-						
+        <p class="card-text">Price:
+			<?php
+
 						$temp = $food-> price;
 						$period = strpos($temp, ".");
 						$length = strlen($temp);
@@ -26,7 +26,7 @@
 							echo '$' . $temp;
 						}
 			?>
-			
+
 		</p>
         {!! Form::open() !!}
         <input id="food_id" name="food_id" type="hidden" value="<?php echo $food->id ?>">
