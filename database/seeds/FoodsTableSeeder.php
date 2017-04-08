23881822475c464943489f4fc2d8c3ef29ec0fb8
bqnguyen94 <ryuu.sakamoto.164@outlook.com>
2017-04-08 20:48:45 +0800

    Add seeds for foods

--- database/seeds/FoodsTableSeeder.php
+++ database/seeds/FoodsTableSeeder.php
@@ -14,6 +14,7 @@ class FoodsTableSeeder extends Seeder
     public function run()
     {
         //
+        /*
         $faker = Faker\Factory::create();
 
         $limit = 50;
@@ -27,5 +28,33 @@ class FoodsTableSeeder extends Seeder
                 'price' => $price
             ]);
         }
+        */
+        Food::create([
+            'restaurant_id' => '1',
+            'name' => 'Doeji Bulgogi',
+            'price' => '4',
+            'image' => "https://www.thefooddictator.com/wp-content/uploads/2016/02/image-18.jpeg",
+        ]);
+
+        Food::create([
+            'restaurant_id' => '1',
+            'name' => 'Dak Bulgogi',
+            'price' => '4',
+            'image' => "http://4.bp.blogspot.com/_UIXOn06Pz70/S85COq9tHxI/AAAAAAAAKCg/-BTJ3EX9vtA/s800/Pineapple+Dak+Bulgogi+(Pineapple+Korean+BBQ+Chicken)+3+500.jpg",
+        ]);
+
+        Food::create([
+            'restaurant_id' => '1',
+            'name' => 'Bulgogi',
+            'price' => '4.5',
+            'image' => "http://www.trifood.com/image/food/bulgogi.JPG",
+        ]);
+
+        Food::create([
+            'restaurant_id' => '1',
+            'name' => 'Saba',
+            'price' => '4.5',
+            'image' => "https://s3.burpple.com/foods/32a0a832d69a0d7312770605_original.?1376242530",
+        ]);
     }
 }
