4c1932e040173a300fd41e202050f6719907dba3
Unknown <astrid2242@gmail.com>
2017-04-09 11:51:09 +0800

    align copyright in footer to be in center

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,15 +2,19 @@
 <div id="ft" class="footer navbar-static-bottom">
 <footer>
 @yield('link')
-  <ul>
- 	<li><a href="http://www.nusfood.com/">NUS Food</a></li>
-    <li><a href="/about">About Us</a></li>
-    <li><a href="/help">Help</a></li>
-    <li class="hidden-xs"><a href="http://steps.comp.nus.edu.sg/">STePS 10</a></li>
-    <li class="hidden-xs"><a href="http://www.comp.nus.edu.sg/~stevenha/cs3226.html#news">CS3226</a></li>
-    
-  </ul>
-	
-	<span id="copyright"> Prototype &copy 2017 NUS Food. All rights reserved.</span>
+    <div class="row">
+      <ul>
+        <li><a href="http://www.nusfood.com/">NUS Food</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/help">Help</a></li>
+        <li class="hidden-xs"><a href="http://steps.comp.nus.edu.sg/">STePS 10</a></li>
+        <li class="hidden-xs"><a href="http://www.comp.nus.edu.sg/~stevenha/cs3226.html#news">CS3226</a></li>
+      </ul>
+    </div>
+
+    <div style="text-align: center">
+        <div style="display: inline-block;"><span id="copyright"> Prototype &copy 2017 NUS Food. All rights reserved.</span></div>
+    </div>
+
  </footer>
 </div>
