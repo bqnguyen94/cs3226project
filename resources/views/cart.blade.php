66ee044e728df5d4d9540d8624a75f4c2f59513b
Julian Chan <frozzenhart@hotmail.com>
2017-04-10 23:57:28 +0800

    More attempted changes

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -118,29 +118,17 @@
                 </div>
             </div>
             <!--todo add in datetime picker-->
+            {!! Form::label('delivery_time','Delivery Time',['class'=>'col-sm-2 control-label']) !!}
             <div class="container">
                 <div class="row">
                     <div class='col-sm-6'>
                         <div class="form-group">
                             <div class='input-group date' id='datetimepicker1'>
-                                <input type='text' class="form-control"/>
-                    <span class="input-group-addon">
+                                {!! Form::text('delivery_time',NULL,['class'=>'form-control']) !!}
+                                <span class="input-group-addon">
                         <span class="glyphicon glyphicon-calendar"></span>
                     </span>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <div class='input-group date' id='datetimepicker1'>
-                        {!! Form::label('delivery_time','Delivery Time',['class'=>'col-sm-2 control-label']) !!}
-                        <div class="col-sm-4">
-                            {!! Form::text('delivery_time',NULL,['class'=>'form-control']) !!}
-                            <span class="input-group-addon">
-	                        <span class="glyphicon glyphicon-calendar"></span>
-	                    </span>
                         </div>
                     </div>
                 </div>
