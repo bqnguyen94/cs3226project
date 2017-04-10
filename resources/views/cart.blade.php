7a9db1efd1135e734be86c0bc23408b799e6ffdb
Julian Chan <frozzenhart@hotmail.com>
2017-04-10 21:51:19 +0800

    Did up labels and form field

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,28 +1,28 @@
 @extends('layouts.template')
 @section('main')
-<head>
-	<link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Vollkorn" rel="stylesheet">
-</head>
-<div id="cart_heading_background" class="col-xs-12">
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Vollkorn" rel="stylesheet">
+    </head>
+    <div id="cart_heading_background" class="col-xs-12">
 
-	 <p id="cart_heading" >Cart</p>
+        <p id="cart_heading">Cart</p>
 
-</div>
-<div class="container">
-    <div class="row">
+    </div>
+    <div class="container">
+        <div class="row">
 
-        <table class="table table-condensed">
-            <thead id="cart_thead">
+            <table class="table table-condensed">
+                <thead id="cart_thead">
                 <tr>
                     <th class="col-xs-1"></th>
                     <th class="col-xs-4">Item name</th>
                     <th class="col-xs-1">Price</th>
                     <th class="col-xs-1">Amount</th>
-					 <th class="col-xs-1"></th>
+                    <th class="col-xs-1"></th>
 
                 </tr>
-            </thead>
-            <tbody id="cart_tbody">
+                </thead>
+                <tbody id="cart_tbody">
                 <?php
                 $i = 0;
                 $user = Auth::user();
@@ -35,25 +35,27 @@
                     <tr class="cart_trow">
                         <td>{{ $i }}</td>
                         <td>{{ $cart_item['food']->name }}</td>
-						<td>${{ number_format($cart_item['food']->price, 2) }}</td>
+                        <td>${{ number_format($cart_item['food']->price, 2) }}</td>
                         <td>{{ $cart_item['amount'] }} </td>
-						<td>
-							{!! Form::open(['route' => 'cart.delete']) !!}
-							<button name="food_id" id="btn-submit" type="submit" class="btn btn-danger" value="<?php echo $cart_item['food_id'] ?>" style="width:24px;height:24px;padding:1px 1px 1px 1px; margin:1px 1px 1px 20px;">
-							  <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-							</button>
-							{!! Form::close() !!}
-						</td>
+                        <td>
+                            {!! Form::open(['route' => 'cart.delete']) !!}
+                            <button name="food_id" id="btn-submit" type="submit" class="btn btn-danger"
+                                    value="<?php echo $cart_item['food_id'] ?>"
+                                    style="width:24px;height:24px;padding:1px 1px 1px 1px; margin:1px 1px 1px 20px;">
+                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            </button>
+                            {!! Form::close() !!}
+                        </td>
 
                     </tr>
                 @endforeach
 
-				<tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-						<td></td>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
 
                 </tr>
                 <tr id="cart_lastrow">
@@ -65,134 +67,132 @@
                     </td>
                     <td></td>
                     <td></td>
-					<td></td>
+                    <td></td>
 
                 </tr>
 
-				     <tr>
+                <tr>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-					<td>{!! Form::open(['route' => 'cart.clear']) !!}
-						<center>
-							<button name="food_id" id="cart_clearcart" type="submit" class="btn btn-danger glyphicon glyphicon-trash">
-								Clear Cart
-							</button>
+                    <td>{!! Form::open(['route' => 'cart.clear']) !!}
+                        <center>
+                            <button name="food_id" id="cart_clearcart" type="submit"
+                                    class="btn btn-danger glyphicon glyphicon-trash">
+                                Clear Cart
+                            </button>
 
 
-						</center>
-						{!! Form::close() !!}</td>
+                        </center>
+                        {!! Form::close() !!}</td>
 
-                	</tr>
-            </tbody>
-        </table>
+                </tr>
+                </tbody>
+            </table>
 
-    </div>
+        </div>
 
-    <div class="row">
-        {!! Form::open() !!}
+        <div class="row">
+            {!! Form::open() !!}
             <div class="form-group">
                 <div class="input-group col-xs-6">
                     <span class="input-group-addon" id="basic-addon2">Deliver to:</span>
-                    <input id="location" name="location" type="text" class="form-control" placeholder="e.g: PGP Residence 4" required/>
+                    <input id="location" name="location" type="text" class="form-control"
+                           placeholder="e.g: PGP Residence 4" required/>
                 </div>
             </div>
             <!--todo add in datetime picker-->
-        <div class="container">
-            <div class="row">
-                <div class='col-sm-6'>
-                    <div class="form-group">
-                        <div class='input-group date' id='datetimepicker1'>
-                            <input type='text' class="form-control" />
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
+            <div class='col-sm-6'>
+                <div class="form-group">
+                    <div class='input-group date' id='datetimepicker1'>
+                        {!! Form::label('delivery_time','Delivery Time',['class'=>'col-sm-2 control-label']) !!}
+                        {!! Form::text('delivery_time',NULL,['class'=>'form-control']) !!}
+                        <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar">
+                        </span>
                     </span>
-                        </div>
                     </div>
+
                 </div>
             </div>
-        </div>
             <div class="form-group">
                 <div class="input_group col-xs-6">
 
                 </div>
             </div>
             <center class="cart_buttons">
-				<a href="/foods" class="btn btn-primary btn-lg" id="cart_continueshopping">Return to Food Menu</a>
-				<button id="btn-submit" class="btn btn-success btn-lg" type="submit">PLACE ORDER</button>
+                <a href="/foods" class="btn btn-primary btn-lg" id="cart_continueshopping">Return to Food Menu</a>
+                <button id="btn-submit" class="btn btn-success btn-lg" type="submit">PLACE ORDER</button>
 
             </center>
-        {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
     </div>
-</div>
-</br></br>
+    </br></br>
 @endsection
 
 <style>
 
-	#cart_heading_background{
+    #cart_heading_background {
 
-		background-image:url("/img/banner/banner2.png");
-		width:100%;
-		max-height:140px;
-		min-height:105px;
-		margin-bottom:40px;
-	}
+        background-image: url("/img/banner/banner2.png");
+        width: 100%;
+        max-height: 140px;
+        min-height: 105px;
+        margin-bottom: 40px;
+    }
 
+    #cart_heading {
 
-	#cart_heading{
+        padding-top: 20px;
+        text-align: 50%;
+        font-size: 48px;
+        text-align: center;
+        color: white;
+        font-weight: 900;
+        font-family: 'Belleza', sans-serif;
+        text-shadow: 2px 2px black;
 
-		padding-top:20px;
-		text-align:50%;
-		font-size:48px;
-		text-align:center;
-		color:white;
-		font-weight:900;
-		font-family:'Belleza', sans-serif;
-		text-shadow:2px 2px black;
+    }
 
+    #cart_thead {
 
+        font-size: 20px;
+        font-family: 'Belleza', sans-serif;
+        font-weight: bold;
 
-	}
+    }
 
-	#cart_thead{
+    .cart_trow td {
 
-		font-size:20px;
-		font-family: 'Belleza', sans-serif;
-		font-weight:bold;
+        border-bottom: 1pt solid #f2f2f2;
 
-	}
+    }
 
-	.cart_trow td{
+    #cart_lastrow td {
 
-		border-bottom:1pt solid #f2f2f2;
+        border-top: 1pt solid #cccccc;
+        border-bottom: 1pt solid #cccccc;
 
-	}
+    }
 
-	#cart_lastrow td{
+    #btn-submit {
+        width: 200px;
+        height: 65px;
+    }
 
-		border-top:1pt solid #cccccc;
-		border-bottom:1pt solid #cccccc;
+    #cart_continueshopping {
+        width: 200px;
+        height: 65px;
+    }
 
-	}
+    #cart_clearcart {
+        width: 150px;
+        height: 50px;
+        padding: 1px 1px 1px 1px;
+        margin: 1px 1px 1px 20px;
 
-	#btn-submit{
-		width:200px;
-		height:65px;
-	}
-
-	#cart_continueshopping{
-		width:200px;
-		height:65px;
-	}
-
-	#cart_clearcart{
-		width:150px;
-		height:50px;
-		padding:1px 1px 1px 1px;
-		margin:1px 1px 1px 20px;
-
-	}
+    }
 
 </style>
