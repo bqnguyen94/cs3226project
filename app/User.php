7d3430b04455e55d769ce51823bc26b255749dca
Julian Chan <frozzenhart@hotmail.com>
2017-04-11 00:49:52 +0800

    More fixes

--- app/User.php
+++ app/User.php
@@ -137,10 +137,11 @@ class User extends Authenticatable
         $user_to_foods = DB::table('user_to_foods')
                 ->where('user_id', $this->id)
                 ->get();
+        $deliveryTime = Carbon::createFromFormat('m/d/Y H:i:s',$deliveryTime)->format('Y-m-d H:i:s');
         $order = Order::create([
             'buyer_id' => $this->id,
             'deliver_location' => $location,
-            'delivery_time' => Carbon::createFromFormat('m/d/Y H:i:s',$deliveryTime)->format('Y-m-d H:i:s');
+            'delivery_time' => $deliveryTime,
         ]);
         foreach ($user_to_foods as $user_to_food) {
             DB::table('order_to_foods')
