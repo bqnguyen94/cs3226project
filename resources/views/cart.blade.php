5b89bdc3ca7e095d4146ce85380b1af3919048a6
Julian Chan <frozzenhart@hotmail.com>
2017-04-10 23:36:54 +0800

    added in attributes for datepicker

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.template')
 @section('link')
-<meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 @stop
 @section('main')
-<head>
-	<link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Vollkorn" rel="stylesheet">
-</head>
-<div id="cart_heading_background" class="col-xs-12">
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Acme|Belleza|Vollkorn" rel="stylesheet">
+    </head>
+    <div id="cart_heading_background" class="col-xs-12">
 
-	 <p id="cart_heading" >Cart</p>
+        <p id="cart_heading">Cart</p>
 
-</div>
-<div class="container">
-    <div class="row">
+    </div>
+    <div class="container">
+        <div class="row">
 
-        <table class="table table-condensed">
-            <thead id="cart_thead">
+            <table class="table table-condensed">
+                <thead id="cart_thead">
                 <tr>
                     <th class="col-xs-1"></th>
                     <th class="col-xs-4">Item name</th>
                     <th class="col-xs-1">Price</th>
-                    <th class="col-xs-1"><center>Amount</center></th>
-					 <th class="col-xs-1"></th>
+                    <th class="col-xs-1">
+                        <center>Amount</center>
+                    </th>
+                    <th class="col-xs-1"></th>
 
                 </tr>
-            </thead>
-            <tbody id="cart_tbody">
+                </thead>
+                <tbody id="cart_tbody">
                 <?php
                 $i = 0;
                 $user = Auth::user();
@@ -38,33 +40,37 @@
                     <tr class="cart_trow">
                         <td>{{ $i }}</td>
                         <td>{{ $cart_item['food']->name }}</td>
-						<td>${{ number_format($cart_item['food']->price, 2) }}</td>
+                        <td>${{ number_format($cart_item['food']->price, 2) }}</td>
                         <td>
-							<center>
-								<span id="{{ $cart_item['food_id'] }}" class="amount-btn" aria-hidden="true" style="cursor: pointer; color: red;"><button class="btn-change">+</button></span>
+                            <center>
+                                <span id="{{ $cart_item['food_id'] }}" class="amount-btn" aria-hidden="true"
+                                      style="cursor: pointer; color: red;"><button class="btn-change">+</button></span>
                                 <a style="text-decoration: none; color: black; cursor: default;display: inline-block;width: 20px;">{{ $cart_item['amount'] }}</a>
-                                <span id="{{ $cart_item['food_id'] }}" class="amount-btn" aria-hidden="true" style="cursor: pointer; color: red;"><button class="btn-change">-</button></span>
-							</center>
-						</td>
-						<td>
-							<center>
-								{!! Form::open(['route' => 'cart.delete']) !!}
-									<button name="food_id" id="btn-submit" type="submit" class="btn btn-danger" value="{{ $cart_item['food_id'] }}" style="width:24px;height:24px;padding:1px 1px 1px 1px; margin:1px 1px 1px 20px;">
-									  	<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-									</button>
-								{!! Form::close() !!}
-							</center>
-						</td>
+                                <span id="{{ $cart_item['food_id'] }}" class="amount-btn" aria-hidden="true"
+                                      style="cursor: pointer; color: red;"><button class="btn-change">-</button></span>
+                            </center>
+                        </td>
+                        <td>
+                            <center>
+                                {!! Form::open(['route' => 'cart.delete']) !!}
+                                <button name="food_id" id="btn-submit" type="submit" class="btn btn-danger"
+                                        value="{{ $cart_item['food_id'] }}"
+                                        style="width:24px;height:24px;padding:1px 1px 1px 1px; margin:1px 1px 1px 20px;">
+                                    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                                </button>
+                                {!! Form::close() !!}
+                            </center>
+                        </td>
 
                     </tr>
                 @endforeach
 
-				<tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-						<td></td>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
 
                 </tr>
                 <tr id="cart_lastrow">
@@ -76,69 +82,70 @@
                     </td>
                     <td></td>
                     <td></td>
-					<td></td>
+                    <td></td>
 
                 </tr>
 
-				     <tr>
+                <tr>
                     <td></td>
                     <td></td>
                     <td></td>
                     <td></td>
-					<td>{!! Form::open(['route' => 'cart.clear']) !!}
-						<center>
-							<button name="food_id" id="cart_clearcart" type="submit" class="btn btn-danger glyphicon glyphicon-trash">
-								Clear Cart
-							</button>
+                    <td>{!! Form::open(['route' => 'cart.clear']) !!}
+                        <center>
+                            <button name="food_id" id="cart_clearcart" type="submit"
+                                    class="btn btn-danger glyphicon glyphicon-trash">
+                                Clear Cart
+                            </button>
 
 
-						</center>
-						{!! Form::close() !!}</td>
+                        </center>
+                        {!! Form::close() !!}</td>
 
-                	</tr>
-            </tbody>
-        </table>
+                </tr>
+                </tbody>
+            </table>
 
-    </div>
+        </div>
 
-    <div class="row">
-        {!! Form::open() !!}
+        <div class="row">
+            {!! Form::open() !!}
             <div class="form-group">
                 <div class="input-group col-xs-6">
                     <span class="input-group-addon" id="basic-addon2">Deliver to:</span>
-                    <input id="location" name="location" type="text" class="form-control" placeholder="e.g: PGP Residence 4" required/>
+                    <input id="location" name="location" type="text" class="form-control"
+                           placeholder="e.g: PGP Residence 4" required/>
                 </div>
             </div>
             <!--todo add in datetime picker-->
-	        <div class="container">
-	            <div class="row">
-	                <div class='col-sm-6'>
-	                    <div class="form-group">
-	                        <div class='input-group date' id='datetimepicker1'>
-	                            <input type='text' class="form-control" />
-	                    <span class="input-group-addon">
-	                        <span class="glyphicon glyphicon-calendar"></span>
-	                    </span>
-	                        </div>
-	                    </div>
-	                </div>
-	            </div>
-	        </div>
             <div class="form-group">
-                <div class="input_group col-xs-6">
-
+                <div class='input-group date' id='datetimepicker1'>
+                    {!! Form::label('delivery_time','Delivery Time',['class'=>'col-sm-2 control-label']) !!}
+                    <div class="col-sm-4">
+                        {!! Form::text('delivery_time',NULL,['class'=>'form-control']) !!}
+                        <span class="input-group-addon">
+	                        <span class="glyphicon glyphicon-calendar">
+                            </span>
+	                    </span>
+                    </div>
                 </div>
             </div>
-            <center class="cart_buttons">
-				<a href="/foods" class="btn btn-primary btn-lg" id="cart_continueshopping">Return to Food Menu</a>
-				<button id="btn-submit" class="btn btn-success btn-lg po" type="submit">PLACE ORDER</button>
+        </div>
+        <div class="form-group">
+            <div class="input_group col-xs-6">
 
-            </center>
+            </div>
+        </div>
+        <center class="cart_buttons">
+            <a href="/foods" class="btn btn-primary btn-lg" id="cart_continueshopping">Return to Food Menu</a>
+            <button id="btn-submit" class="btn btn-success btn-lg po" type="submit">PLACE ORDER</button>
+
+        </center>
         {!! Form::close() !!}
     </div>
-</div>
-</br></br>
+    </div>
+    </br></br>
 @endsection
 @section('script')
-<script type="text/javascript" src="/js/cart.js"></script>
+    <script type="text/javascript" src="/js/cart.js"></script>
 @endsection
